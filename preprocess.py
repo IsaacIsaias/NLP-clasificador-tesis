64b7fbca974c5a5c32157a0efb3081ba7d90d3a4
yiselclavel <yclavelq@uho.edu.cu>
2022-04-03 18:09:00 -0400

    Add processed dataset

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import re
 import string
-
+import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -8,6 +8,7 @@ from nltk.stem.snowball import SnowballStemmer
 from datasets import list_datasets, load_dataset
 
 # nltk.download('punkt')
+
 
 # cargar dataset de huggingface
 def cargar_dataset (name_dataset):
@@ -23,8 +24,18 @@ def cargar_dataset (name_dataset):
     return train_ds
 
 
+# cargar dataset en formato csv
+def cargar_dataset_csv ():
+    dataframe = pd.DataFrame()
+    dataframe = pd.read_csv('./datasets/dataset_tesis.csv', encoding='ISO-8859-1', sep='|', engine='python')
+    dataframe.columns = ['texto','autor_nombre','autor_apellido','titulo','año','carrera']
+    print(dataframe.head())
+    print(dataframe.columns)
+    return dataframe
+
+
 def preprocess (text):
-    text = "A las 8 en punto de la mañana ... Arthur no se sentía bien. 21 18-5-2022 yisel@gmail.com"
+    #text = "A las 8 en punto de la mañana ... Arthur no se sentía bien. 21 18-5-2022 yisel@gmail.com"
     text = text.lower()  # convertir a minúsculas
 
     # tokenizar
@@ -41,29 +52,53 @@ def preprocess (text):
     # eliminar palabras vacías
     # print(stopwords.words('spanish')) #lista de palabras vacías
     stop_words = set(stopwords.words('spanish'))
-    filtered_sentence = [w for w in words if not w.lower() in stop_words]
-    filtered_sentence = []
+    #clean_words = [w for w in words if not w.lower() in stop_words]
+    clean_words = []
     for w in words:
         if w not in stop_words:
-            filtered_sentence.append(w)
-    print(filtered_sentence)
+            clean_words.append(w)
+    print(clean_words)
 
     # stemming: eliminar plurales
     stemmer = SnowballStemmer("spanish")
-    singles_words = [stemmer.stem(plural) for plural in words]
+    singles_words = [stemmer.stem(plural) for plural in clean_words]
     print(singles_words)
     return singles_words
 
 
-train_ds = cargar_dataset('tesis')
-texto = train_ds[:'Texto']
-titulo = train_ds[:'Título']
-carrera = train_ds[:'Carrera']
-texto_procesado = preprocess(texto)
-titulo_procesado = preprocess(titulo)
+# PROCESAR EL DATASET:
 
-file = open('', 'w', encoding='ISO-8859-1')
-file.write('introduccion,titulo,carrera' + '\n')
-for i in range(len(texto_procesado)):
-    file.write(str(texto_procesado[i]) + ',' + str(titulo_procesado[i]) + ',' + str(carrera[i]) + '\n')
+# leer datset de huggingface
+# train_ds = cargar_dataset('tesis')
+# texto = train_ds[:'texto']
+# titulo = train_ds[:'titulo']
+# carrera = train_ds[:'carrera']
+
+# leer dataset en csv
+df = cargar_dataset_csv()
+texto = df.loc[:, 'texto']
+titulo = df.loc[:, 'titulo']
+carrera = df.loc[:, 'carrera']
+textos_procesados = []
+titulos_procesados = []
+
+for i in range(len(texto)):
+    #print(texto[i])
+    textos_procesados.append(preprocess(texto[i]))
+
+for j in range(len(titulo)):
+    #print(texto[j])
+    titulos_procesados.append(preprocess(titulo[j]))
+
+file = open('datasets/dataset_tesis_procesado.csv', 'w', encoding='ISO-8859-1')
+file.write('texto|titulo|carrera' + '\n')
+for k in range(len(textos_procesados)):
+    for a in textos_procesados[k]:
+        #print(a)
+        file.write(str(a) + ' ')
+    file.write('|')
+    for b in titulos_procesados[k]:
+        #print(b)
+        file.write(str(b) + ' ')
+    file.write('|' + str(carrera[k]) + '\n')
 file.close()
